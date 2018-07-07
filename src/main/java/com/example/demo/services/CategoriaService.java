631764b6697f04460cec6f121d6553e16caf0b5e
Vitor Hugo <vitinsk@hotmail.com>
2018-07-07 15:06:17 -0300

    validação com BEAN validation

--- src/main/java/com/example/demo/services/CategoriaService.java
+++ src/main/java/com/example/demo/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dto.CategoriaDTO;
 import com.example.demo.model.Categoria;
 import com.example.demo.repository.CategoriaRepository;
 import com.example.demo.services.exception.DataIntegrityException;
@@ -54,6 +55,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
