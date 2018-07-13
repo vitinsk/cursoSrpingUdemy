70c0d0f64b16e18e15f097ba2454f201953cf115
Vitor Hugo <vitinsk@hotmail.com>
2018-07-13 15:57:23 -0300

    profile de desenvolvimento

---
+++ src/main/java/com/example/demo/config/DevConfig.java
@@ -0,0 +1,31 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.example.demo.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+@Autowired
+private DBService dbService;
+	
+@Value("${spring.jpa.hibernate.ddl-auto}")
+private String strategy;
+
+	@Bean
+	public boolean instantiateDatabase() throws Exception {
+		
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		
+		dbService.instantiateTesteDatabase();
+		return true;
+		
+	}
+}
